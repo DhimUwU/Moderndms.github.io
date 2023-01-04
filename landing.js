bb92d8c579c5110a1c31f05340657f1fd20a3c08
dhimasaww <dhimasfly1@gmail.com>
2023-01-04 12:23:26 +0700

    first commit

---
+++ landing.js
@@ -0,0 +1,32 @@
+let menu = document.querySelector('#menu-bar');
+let nav = document.querySelector('.nav');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bi-x-octagon');
+    nav.classList.toggle('active');
+}
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll = () =>{
+
+    menu.classList.remove('bi-x-octagon');
+    nav.classList.remove('active');
+
+    section.forEach(sec =>{
+
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header .nav a[href*='+id+']').classList.add('active');
+            });
+        };
+    });
+
+}
